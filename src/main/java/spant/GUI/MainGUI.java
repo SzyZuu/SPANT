f1ab9bd05a008cf297bc6700ee25f8403553f267
SzyZu <skwarek116@gmail.com>
2024-01-21 19:08:07 +0100

    feat: updated MainGUI fix: fixed lookandfeel not showing on all panels

--- src/main/java/spant/GUI/MainGUI.java
+++ src/main/java/spant/GUI/MainGUI.java
@@ -1,12 +1,45 @@
 package spant.GUI;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class MainGUI extends JPanel {
+    JTextField textField;
     public MainGUI(){
         doGUI();
     }
     public void doGUI(){
+        FlatDarkLaf.setup();
+        setBorder(new EmptyBorder(5, 5, 5, 5));
+        setLayout(new GridLayout(2, 1, 0, 0));
 
+        JLabel spantLabel = new JLabel("SPANT");
+        spantLabel.setForeground(new Color(192, 192, 192));
+        spantLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        spantLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(spantLabel);
+
+        JPanel bottomPanel = new JPanel();
+        add(bottomPanel);
+        bottomPanel.setLayout(new GridLayout(2, 0, 0, 0));
+
+        JPanel textPanel = new JPanel();
+        bottomPanel.add(textPanel);
+
+        textField = new JTextField();
+        textPanel.add(textField);
+        textField.setColumns(20);
+
+        JPanel buttonPanel = new JPanel();
+        bottomPanel.add(buttonPanel);
+
+        JButton btnNewButton = new JButton("New button");
+        JButton btnNewButton_1 = new JButton("New button");
+
+        buttonPanel.add(btnNewButton);
+        buttonPanel.add(btnNewButton_1);
     }
 }
--- src/main/java/spant/GUI/UIWindow.java
+++ src/main/java/spant/GUI/UIWindow.java
@@ -3,32 +3,22 @@ package spant.GUI;
 import com.formdev.flatlaf.FlatDarkLaf;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
 
-public class UIWindow extends JFrame {
-
-    private JPanel contentPane;
+public class UIWindow {
+    JFrame window;
     MainGUI mainGUI = new MainGUI();
     SpantGUI spantGUI = new SpantGUI();
     public UIWindow() {
+        window = new JFrame();
         FlatDarkLaf.setup();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(450, 300);
-        setLocationRelativeTo(null);
-        setTitle("SPANT");
-        setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(450, 300);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setTitle("SPANT");
 
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setBackground(UIManager.getColor("control"));
-        setContentPane(contentPane);
-        contentPane.setLayout(new GridLayout(2, 1, 0, 0));
-
-        JLabel lblNewLabel = new JLabel("SPANT");
-        lblNewLabel.setForeground(new Color(192, 192, 192));
-        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
-        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        contentPane.add(lblNewLabel);
+        window.setContentPane(mainGUI);
+        window.getContentPane().setBackground(UIManager.getColor("control"));
+        window.setVisible(true);
     }
 }
