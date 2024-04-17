c2ef58cb648503dc8c04d82342e2a2dd7fb2983d
SzyZu <skwarek116@gmail.com>
2024-04-17 17:12:15 +0200

    refactor: updated info label

--- src/main/java/spant/GUI/SpantGUI.java
+++ src/main/java/spant/GUI/SpantGUI.java
@@ -7,11 +7,9 @@ import java.awt.*;
 public class SpantGUI extends JPanel {
     public SpantGUI(){
         setBorder(new EmptyBorder(5, 5, 5, 5));
-        setLayout(new GridLayout(4, 1, 0, 0));
+        setLayout(new GridLayout(3, 1, 0, 0));
 
         JLabel empty = new JLabel();
-        add(empty);
-        add(empty);
 
         JLabel streamerLabel = new JLabel("Streamer live!");
         streamerLabel.setForeground(new Color(192, 192, 192));
@@ -19,11 +17,18 @@ public class SpantGUI extends JPanel {
         streamerLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(streamerLabel);
 
-        JLabel infoLabel = new JLabel("<html>Your PC will turn off automatically after the stream ends, with a small delay because of Twitch's API. To cancel, just close this window.<html/>");
+        JLabel infoLabel = new JLabel("<html>Your PC will turn off automatically after the stream ends, with a small delay because of Twitch's API. To cancel, press the exit button.<html/>");
         infoLabel.setBorder(new EmptyBorder(0, 20, 5, 20));
         infoLabel.setForeground(new Color(160, 160, 160));
         infoLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
         infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(infoLabel);
+
+        JLabel infoLabel2 = new JLabel("<html>You can close this window and the program will continue working in system tray.<html/>");
+        infoLabel2.setBorder(new EmptyBorder(0, 20, 5, 20));
+        infoLabel2.setForeground(new Color(160, 160, 160));
+        infoLabel2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        infoLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+        add(infoLabel2);
     }
 }
