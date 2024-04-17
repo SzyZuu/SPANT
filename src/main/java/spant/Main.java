65f534c6d25f70e02a0338bdfb690b796ccbdd44
SzyZu <skwarek116@gmail.com>
2024-04-17 16:46:09 +0200

    feat: TrayIcon on close instead of exiting program

--- src/main/java/spant/Main.java
+++ src/main/java/spant/Main.java
@@ -4,10 +4,15 @@ import com.formdev.flatlaf.FlatDarkLaf;
 import spant.GUI.MainGUI;
 import spant.GUI.SpantGUI;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 import java.util.Objects;
-import java.util.Optional;
 
 public class Main {
 
@@ -17,6 +22,7 @@ public class Main {
     StreamCheck streamCheck;
     CardLayout cardLayout;
     JPanel mainPanel;
+    Image icon;
 
     public Main() {
         FlatDarkLaf.setup();
@@ -25,12 +31,27 @@ public class Main {
         mainGUI = new MainGUI(this);
         spantGUI = new SpantGUI();
         window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            icon = ImageIO.read(Objects.requireNonNull(getClass().getResource("/SPANT.png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                window.setVisible(false);
+                doSystemTray();
+            }
+        });
+
         window.setSize(450, 300);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
         window.setTitle("SPANT");
-        window.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("/SPANT.png"))).getImage());
+        window.setIconImage(icon);
 
         mainPanel = new JPanel();
         mainPanel.setLayout(cardLayout);
@@ -55,5 +76,33 @@ public class Main {
     public void changePane(){
         System.out.println("switching");
         cardLayout.show(mainPanel, "spant");
+    }
+
+    public void doSystemTray(){
+        if (!SystemTray.isSupported()) {
+            System.err.println("System Tray not supported");
+            return;
+        }
+
+        final PopupMenu popUp = new PopupMenu();
+
+        SystemTray tray = SystemTray.getSystemTray();
+        TrayIcon trayIcon = new TrayIcon(icon, "SPaNT");
+
+        MenuItem exitItem = new MenuItem("Exit SPaNT");
+        popUp.add(exitItem);
+
+        trayIcon.setPopupMenu(popUp);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.err.println("Error adding tray icon: " + e.getMessage());
+        }
+
+        exitItem.addActionListener(e -> {
+            tray.remove(trayIcon);
+            System.exit(0);
+        });
     }
 }
